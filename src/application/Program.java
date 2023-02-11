c2585ffe7725b8844cc9fdc94ea6eda5b8ec0e51
Gabriel Ventorim <gabriel.ventorims@gmail.com>
2023-02-11 20:23:07 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,12 @@
 package application;
 
-public class Program {
+import java.util.Locale;
+import java.util.Scanner;
 
+public class Program {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner in = new Scanner(System.in);
+			
+	}
 }
---
+++ src/boardgame/Position.java
@@ -0,0 +1,33 @@
+package boardgame;
+
+public class Position {
+	private Integer row;
+	private Integer column;
+	
+	public Position(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	public Position () {
+	}
+	
+	public Integer getRow() {
+		return row;
+	}
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+	public Integer getColumn() {
+		return column;
+	}
+	public void setColumn(Integer column) {
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return row + " ," + column;
+	}
+	
+}
