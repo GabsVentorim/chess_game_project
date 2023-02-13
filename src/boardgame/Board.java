1aefb92a09dc6c2ad816ad68d86304819b47f903
Gabriel Ventorim <gabriel.ventorims@gmail.com>
2023-02-13 09:20:51 -0300

    Saving some small changes

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -24,17 +24,23 @@ public class Board {
 	}
 
 	public Piece piece(int row, int column) {
+		if (!positionExists(row, column)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[row][column];
 	}
 
 	public Piece piece(Position position) {
-		if (!positionExists(row,column)) {
-			
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
 		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("There is already a piece on position " + position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position; // protected
 	}
@@ -47,7 +53,10 @@ public class Board {
 		return positionExists(position.getRow(), position.getColumn());
 	}
 
-	public boolean thereIsAPiece(Position positon) {
-		return piece(positon) != null;
+	public boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		return piece(position) != null;
 	}
 }
