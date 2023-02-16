a000a17c15071ce1367c69c0fef29c166477a1ed
Gabriel Ventorim <gabriel.ventorims@gmail.com>
2023-02-16 19:59:29 -0300

    Scanner

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,7 +14,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
@@ -24,14 +24,14 @@ public class Program {
 				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.print("Source: ");
-				ChessPosition source = UI.readChessPosition(sc);
+				ChessPosition source = UI.readChessPosition(in);
 				
 				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				System.out.println();
 				System.out.print("Target: ");
-				ChessPosition target = UI.readChessPosition(sc);
+				ChessPosition target = UI.readChessPosition(in);
 				
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 				
@@ -41,21 +41,21 @@ public class Program {
 				
 				if (chessMatch.getPromoted() != null) {
 					System.out.print("Enter piece for promotion (B/N/R/Q): ");
-					String type = sc.nextLine().toUpperCase();
+					String type = in.nextLine().toUpperCase();
 					while (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
 						System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
-						type = sc.nextLine().toUpperCase();
+						type = in.nextLine().toUpperCase();
 					}
 					chessMatch.replacePromotedPiece(type);
 				}
 			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
-				sc.nextLine();
+				in.nextLine();
 			}
 			catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
-				sc.nextLine();
+				in.nextLine();
 			}
 		}
 		UI.clearScreen();
